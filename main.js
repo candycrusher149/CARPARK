425824a6e4a08650dd01492303734fed847187fd
Me <83992410+candycrusher149@users.noreply.github.com>
2021-09-16 20:15:56 -0400

    Add files via upload
    carpark2.0

--- main.js
+++ main.js
@@ -13,20 +13,20 @@ greencar_y = 225;
 function add() {
 	background_imgtag = new Image();
 	background_imgtag.onload = uploadBackground;
-	background_imgtag.src = bacground_image();
+	background_imgtag.src = background_image;
 	greencar_imgtag = new Image();
 	greencar_imgtag.onload = uploadgreencar;
-	greencar_imgtag.src = greencar_image();
+	greencar_imgtag.src = greencar_image;
 
 
 }
 
 function uploadBackground() {
-	ctx.drawImage(background_imgtag 0,0, canvas.width, canvas.height);
+	ctx.drawImage(background_imgtag, 0,0, canvas.width, canvas.height);
 }
 
 function uploadgreencar() {
-	ctx.drawImage(greencar_imgtag greencar_x,geencar_y, greencar_width, greencar_height);
+	ctx.drawImage(greencar_imgtag, greencar_x,geencar_y, greencar_width, greencar_height);
 }
 
 
@@ -76,7 +76,7 @@ if(greencar_y >=0)
 
 function down()
 {
-	if(greencar_y >=500)
+	if(greencar_y >=350)
 	{
 		greencar_y = greencar_y +10;
 		console.log("when down arrow is pressed x = " + greencar_x +" | y = "+ greencar_y)
@@ -87,22 +87,21 @@ function down()
 
 function left()
 {
-	//if(greencar_y >=0)
+	if(greencar_x >=0)
 {
-	greencar_y = greencar_y -10;
+	greencar_x = greencar_x -10;
 	console.log("when left arrow is pressed x = " + greencar_x +" | y = "+ greencar_y)
 	uploadBackground();
 	uploadgreencar()
 }
-}
 
 function right()
 {
-	if(greencar_y >=700)
+	if(greencar_x >=700)
 {
-	greencar_y = greencar_y +10;
+	greencar_x = greencar_x +10;
 	console.log("when right arrow is pressed x = " + greencar_x +" | y = "+ greencar_y)
 	uploadBackground();
 	uploadgreencar()
-}//Define function to move the car right side
+}
 }
